17b71d516db546a64e3e1020f7dea699f30281d5
Emre Portakal <emre.portakal@commencis.com>
2024-08-08 13:22:07 +0300

    feat: read any pdf file and split in to chunks

---
+++ chatbot.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+# upload PDF files
+
+st.header("My first chatbot")
+
+with st.sidebar:
+    st.title("your documents")
+    file = st.file_uploader("upload a pdf file and start asking questions", type="pdf")
+
+# extract the text
+if file is not None:
+    pdf_reader = PdfReader(file)
+    text = ""
+    for page in pdf_reader.pages:
+        text +=page.extract_text()
+
+# break it into chunks
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators=list("\n"),
+        chunk_size=1000,
+        chunk_overlap=150,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    st.write(chunks)
+
+
+
